73cf36249a6bc5f3862d1a921900096e69554551
andrefmrocha <andrefmrocha@live.com.pt>
2018-03-06 12:21:31 +0000

    Added  exercise 3.1

--- Capitulo 3/main.cpp
+++ Capitulo 3/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int exercicio_1()
+double area(float x_a, float x_b, float x_c,float y_a,float y_b,float y_c)
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    double a, b, c, s;
+    a=sqrt(pow((x_a-x_b),2)+pow((y_a-y_b),2));
+    b=sqrt(pow((x_c-x_b),2)+pow((y_c-y_b),2));
+    c=sqrt(pow((x_a-x_c),2)+pow((y_a-y_c),2));
+    s=(a+b+c)/2;
+    s=sqrt(s*(s-a)*(s-b)*(s-c));
+    return s;
+}
+
+void exercicio_1()
+{
+    float x_a, x_b, x_c, y_a, y_b, y_c;
+    double s;
+    cout << "Coordenadas de a? ";
+    cin >> x_a;
+    cin >> y_a;
+    cout << "Coordenadas de b? ";
+    cin >> x_b;
+    cin >> y_b;
+    cout << "Coordenadas de c? ";
+    cin >> x_c;
+    cin >> y_c;
+    s= area(x_a,y_a,x_b,y_b,x_c,y_c);
+    cout << "A área é " << s;
 }
 
 int main()
