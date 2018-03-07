a0c32a1c193294c85ddeef8c4b342b5846270bd7
andrefmrocha <andrefmrocha@live.com.pt>
2018-03-07 09:53:02 +0000

    Added 3.5

--- Capitulo 3/main.cpp
+++ Capitulo 3/main.cpp
@@ -69,6 +69,199 @@ void exercicio_4()
 
 }
 
+bool readFracc(int numerator, int denominator)
+{
+    bool flag=false;
+    if (denominator==0||numerator==0)
+    {
+        flag=true;
+    }
+    return flag;
+}
+
+void exercicio_5_a()
+{
+    int numerator, denominator;
+    bool flag;
+    cout << "Indique o valor do numerador e do denominador, respetivamente: ";
+    cin >> numerator >> denominator;
+    flag=readFracc(numerator, denominator);
+    if(numerator%denominator==0)
+    {
+        flag=true;
+    }
+    if(flag)
+    {
+        cout << "Não é uma fraccao";
+    } else
+    {
+        cout << "É uma fraccao";
+    }
+}
+
+void writeFacc(int numerator, int denominator)
+{
+    cout << numerator << "/"<< denominator;
+}
+
+void exercicio_5_b()
+{
+    int numerator, denominator;
+    bool flag;
+    cout << "Indique o valor do numerador e do denominador, respetivamente: ";
+    cin >> numerator >> denominator;
+    flag=readFracc(numerator, denominator);
+    if(flag)
+    {
+        cout << "Nao e uma fraccao";
+    }
+    else if(numerator%denominator==0)
+    {
+        cout << numerator/denominator;
+    }
+    else
+    {
+        writeFacc(numerator, denominator);
+    }
+}
+
+int reduceFracc(int &numerator, int &denominator)
+{
+    int numerador=numerator, denominador=denominator;
+    while (true)
+    {
+        numerador=numerador%denominador;
+        if(numerador==0)
+        {
+            numerador = denominador;
+            break;
+        }
+        denominador=denominador%numerador;
+        if(denominador==0)
+        {
+            break;
+        }
+    }
+    return numerador;
+}
+
+int leituraNum()
+{
+    int numerator;
+    cin >> numerator;
+    return numerator;
+}
+void exercicio_5_c()
+{
+    int numerator, denominator, gcd;
+    bool flag;
+    cout << "Indique o valor do numerador e do denominador, respetivamente: ";
+    cin >> numerator >> denominator;
+    flag=readFracc(numerator, denominator);
+    if(flag)
+    {
+        cout << "Nao e uma fraccao";
+    }
+    else if(numerator%denominator==0)
+    {
+        cout << numerator/denominator;
+    }
+    else
+    {
+        gcd=reduceFracc(numerator, denominator);
+        numerator=numerator/gcd;
+        denominator=denominator/gcd;
+        writeFacc(numerator, denominator);
+    }
+
+}
+
+void multFracc(int numerador_a, int denominador_a, int numerador_b, int denominador_b)
+{
+    int numerador_f, denominador_f;
+    reduceFracc(numerador_a, denominador_a);
+    reduceFracc(numerador_b, denominador_b);
+    numerador_f=numerador_a*numerador_b;
+    denominador_f=denominador_a*denominador_b;
+    reduceFracc(numerador_f, denominador_f);
+    cout << numerador_a << "/" << denominador_a << "*" << numerador_b << "/" << denominador_b << "=";
+    cout << numerador_f << "/" << denominador_f;
+}
+void divFracc(int numerador_a, int denominador_a, int numerador_b, int denominador_b)
+{
+    int numerador_f, denominador_f;
+    reduceFracc(numerador_a, denominador_a);
+    reduceFracc(numerador_b, denominador_b);
+    numerador_f=numerador_a*denominador_b;
+    denominador_f=numerador_b*denominador_a;
+    reduceFracc(numerador_f, denominador_f);
+    cout << numerador_a << "/" << denominador_a << "/" << numerador_b << "/" << denominador_b << "=";
+    cout << numerador_f << "/" << denominador_f;
+}
+void somaFracc(int numerador_a, int denominador_a, int numerador_b, int denominador_b)
+{
+    int numerador_f, denominador_f;
+    reduceFracc(numerador_a, denominador_a);
+    reduceFracc(numerador_b, denominador_b);
+    denominador_f=denominador_a*denominador_b;
+    numerador_a=numerador_a*denominador_b;
+    numerador_b=numerador_b*denominador_a;
+    numerador_f=numerador_a+numerador_b;
+    reduceFracc(numerador_a, denominador_a);
+    reduceFracc(numerador_b, denominador_b);
+    reduceFracc(numerador_f,denominador_f);
+    cout << numerador_a << "/" << denominador_a << "+" << numerador_b << "/" << denominador_b << "=";
+    cout << numerador_f << "/" << denominador_f;
+
+}
+void subFracc(int numerador_a, int denominador_a, int numerador_b, int denominador_b)
+{
+    int numerador_f, denominador_f;
+    reduceFracc(numerador_a, denominador_a);
+    reduceFracc(numerador_b, denominador_b);
+    denominador_f=denominador_a*denominador_b;
+    numerador_a=numerador_a*denominador_b;
+    numerador_b=numerador_b*denominador_a;
+    numerador_f=numerador_a-numerador_b;
+    reduceFracc(numerador_a, denominador_a);
+    reduceFracc(numerador_b, denominador_b);
+    reduceFracc(numerador_f,denominador_f);
+    cout << numerador_a << "/" << denominador_a << "-" << numerador_b << "/" << denominador_b << "=";
+    cout << numerador_f << "/" << denominador_f;
+
+}
+
+
+void exercicio_5_d()
+{
+    int numerador_a, numerador_b, denominador_a, denominador_b;
+    char operacao;
+    cout << "Introduza o valor do primeiro numerador: ";
+    numerador_a=leituraNum();
+    cout << "Introduza o valor do primeiro denominador: ";
+    denominador_a=leituraNum();
+    cout << "Introduza o valor do segundo numerador: ";
+    numerador_b=leituraNum();
+    cout << "Introduza o valor do segundo denominador";
+    denominador_b=leituraNum();
+    cout << "Introduza o símbolo aritmetico da operaçao que quer realizar ";
+    switch(operacao)
+    {
+        case '+':
+            somaFracc(numerador_a,denominador_a,numerador_b,denominador_b);
+            break;
+        case '-':
+            subFracc(numerador_a,denominador_a,numerador_b,denominador_b);
+            break;
+        case '*':
+            multFracc(numerador_a,denominador_a,numerador_b,denominador_b);
+            break;
+        case '/':
+            divFracc(numerador_a,denominador_a,numerador_b,denominador_b);
+            break;
+
+    }
+}
 int main()
 {
     int exercicio;
@@ -85,6 +278,18 @@ int main()
         case 4:
             exercicio_4();
             break;
+        case 51:
+            exercicio_5_a();
+        case 52:
+            exercicio_5_b();
+            break;
+        case 53:
+            exercicio_5_c();
+            break;
+        case 54:
+            exercicio_5_d();
+            break;
+
         default:
             cout << "Exercicio nao definido";
 
