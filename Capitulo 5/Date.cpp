34531f4bd2d06136fa23300c9db3d74fb6731748
andrefmrocha <andrefmrocha@live.com.pt>
2018-04-18 09:49:16 +0100

    Completed exercise 1  - Chapter 5

--- Capitulo 5/Date.cpp
+++ Capitulo 5/Date.cpp
@@ -22,17 +22,17 @@ string Date::getDate()
     return date;
 }
 
-unsigned int Date::getDay()
+unsigned int Date::getDay() const
 {
     return day;
 }
 
-unsigned int Date::getMonth()
+unsigned int Date::getMonth() const
 {
     return month;
 }
 
-unsigned int Date::getYear()
+unsigned int Date::getYear() const
 {
     return year;
 }
@@ -62,4 +62,162 @@ void Date::setYear(unsigned int year)
 void Date::show()
 {
     cout << getDate() << endl;
+}
+bool Date::isLeap()
+{
+    if((year % 4 == 0 && year % 100 != 0)|| year % 100 == 0)
+        return true;
+    else
+        return false;
+}
+
+bool Date::isValid()
+{
+    if(month == 0 || month > 12 || year < 0 || day > 31 || day == 0)
+        return false;
+    else if(month == 2)
+    {
+        if(isLeap())
+        {
+            if(day <= 29)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        else
+        {
+
+            if(day <= 28)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    else
+    {
+        switch (month)
+        {
+            case 1:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+            case 3:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+            case 4:
+                if(day <= 30)
+                    return true;
+                else
+                    return false;
+            case 5:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+            case 6:
+                if(day <= 30)
+                    return true;
+                else
+                    return false;
+            case 7:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+            case 8:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+            case 9:
+                if(day <= 30)
+                    return true;
+                else
+                    return false;
+            case 10:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+            case 11:
+                if(day <= 30)
+                    return true;
+                else
+                    return false;
+            case 12:
+                if(day <= 31)
+                    return true;
+                else
+                    return false;
+        }
+    }
+
+}
+
+bool Date::isEqualTo(const Date &d1) const
+{
+    if(year == d1.getYear() && month == d1.getMonth() && day == d1.getDay())
+        return true;
+    else
+        return false;
+}
+
+bool Date::isNotEqualTo(const Date &d1) const
+{
+    if(year == d1.getYear() && month == d1.getMonth() && day == d1.getDay())
+        return false;
+    else
+        return true;
+}
+
+bool Date::isAfter(const Date &d1) const
+{
+    if(year > d1.getYear())
+        return true;
+    else if(year == d1.getYear())
+    {
+        if(month > d1.getMonth())
+            return true;
+        else if(month == d1.getMonth())
+        {
+            if(day > d1.getDay())
+            {
+                return false;
+            }
+            return false;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+}
+
+bool Date::isBefore(const Date &d1) const
+{
+    if(isAfter(d1))
+        return false;
+    else if(isNotEqualTo(d1))
+        return true;
+}
+
+Date::Date()
+{
+    time_t now = time(0);
+    tm *now_time = localtime(&now);
+    year = now_time->tm_year + 1900;
+    month = now_time->tm_mon + 1;
+    day = now_time->tm_mday;
 }
--- Capitulo 5/Date.h
+++ Capitulo 5/Date.h
@@ -7,23 +7,31 @@
 
 #include <string>
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 class Date
 {
 public:
+    Date();
     Date(unsigned int year, unsigned int month,unsigned int day);
     Date(string yearMonthDay); // yearMonthDay must be in format "yyyy/mm/dd"
     void setYear(unsigned int year) ;
     void setMonth(unsigned int month) ;
     void setDay(unsigned int day) ;
     void setDate(unsigned int year, unsigned int month, unsigned int day) ;
-    unsigned int getYear() ;
-    unsigned int getMonth() ;
-    unsigned int getDay() ;
+    unsigned int getYear() const;
+    unsigned int getMonth() const;
+    unsigned int getDay() const;
     string getDate(); // returns the date in format "yyyy/mm/dd"
     void show(); // shows the date on the screen in format "yyyy/mm/dd"
+    bool isValid();
+    bool isEqualTo(const Date &d1) const;
+    bool isNotEqualTo(const Date &d1) const;
+    bool isAfter(const Date &d1) const;
+    bool isBefore(const Date &d1) const;
 private:
+    bool isLeap();
     unsigned int year;
     unsigned int month;
     unsigned int day;
